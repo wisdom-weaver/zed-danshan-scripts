9b9a961bd4e7a24c38e25d69f8e8a7724e37d2ed
wisdom-weaver <danish.ansari.nox@gmail.com>
2021-10-03 18:58:48 +0530

    studs 2

--- js_code_files/zed_api_cache_script.js
+++ js_code_files/zed_api_cache_script.js
@@ -108,65 +108,90 @@ const test_api = (z) => `https://jsonplaceholder.typicode.com/todos/${z}`;
 
 const studs_api = (z) =>
   `https://api.zed.run/api/v1/stud/horses?offset=0&gen[]=${z}&gen[]=${z}`;
-const studs_api_cacher = async () => {
-  // let apis = [1, 2, 3].map((z) => test_api(z));
-  let apis = [1].map((z) => studs_api(z));
 
-  let prev_datas = await Promise.all(
-    apis.map((api) =>
-      zed_db.db.collection("zed_api_cache").findOne({ id: api })
-    )
-  );
-  let datas = await Promise.all(apis.map((api) => fetch_a(api)));
-  let db_date = new Date().toISOString();
+let process_prev_curr_studs = (prev, curr) => {
+  if (_.isEmpty(prev)) prev = { new: [], old: [] };
+  let new_horses = [];
+  let old_horses = [];
+  let prevsno = [...prev.new, ...prev.old];
+  for (let h of curr.new) {
+    let { hid, date } = h;
+    if (_.map(prevsno, "hid").includes(hid) == false) new_horses.push(h);
 
-  for (let [i, doc] of _.entries(prev_datas)) {
-    if (_.isEmpty(doc)) {
-      console.log("ERROR prev_data", apis[i]);
-      continue;
+    if (_.map(prev.new, "hid").includes(hid)) {
+      let { date: dpn } = _.find(prev.new, { hid });
+      if (new Date(date).getTime() - new Date(dpn).getTime() < 10 * 60000)
+        new_horses.push({ ...h, date: dpn });
+      else old_horses.push({ ...h, date: dpn });
     }
-    write_to_path({
-      file_path: `${appRootPath}/data/studs/prev_data-${apis[i]}.json`,
-      data: doc,
-    });
-    console.log("SUCCESS prev_data", apis[i]);
-  }
-  for (let [i, data] of _.entries(datas)) {
-    if (_.isEmpty(data)) {
-      console.log("ERROR data", apis[i]);
-      continue;
-    }
-    data = struct_studs_api_data(data);
-    let doc = { id: apis[i], data, db_date };
-    write_to_path({
-      file_path: `${appRootPath}/data/studs/data-${apis[i]}`,
-      data: doc,
-    });
-    console.log("SUCCESS data", apis[i]);
-  }
 
-  // let bulk = [];
-  // for (let [i, data] of _.entries(datas)) {
-  //   if (_.isEmpty(data)) {
-  //     console.log("ERROR", apis[i]);
-  //     continue;
-  //   }
-  //   data = struct_studs_api_data(data);
-  //   let doc = { id: apis[i], data, db_date };
-  //   console.log("SUCCESS", apis[i]);
-  //   bulk.push({
-  //     updateOne: {
-  //       filter: { id: apis[i] },
-  //       update: { $set: doc },
-  //       upsert: true,
-  //     },
-  //   });
-  // }
-  // if (_.isEmpty(bulk)) return;
-  // await zed_db.db.collection("zed_api_cache").bulkWrite(bulk);
-  // console.log("# studs_api_cacher", bulk.length, "written");
+    if (_.map(prev.old, "hid").includes(hid)) {
+      let { date: dpo } = _.find(prev.old, { hid });
+      if (new Date(date).getTime() - new Date(dpo).getTime() < 24 * 60 * 60000)
+        old_horses.push({ ...h, date: dpo });
+    }
+  }
+  console.log("new:", _.map(new_horses, "hid"));
+  console.log("old:", _.map(old_horses, "hid"));
+  return { new: new_horses, old: old_horses };
+};
+const studs_api_cacher_test = async () => {
+  let a = read_from_path({
+    file_path: `${appRootPath}/data/studs/data_z1_01.json`,
+  });
+  let b = read_from_path({
+    file_path: `${appRootPath}/data/studs/data_z1_02.json`,
+  });
+  let c = read_from_path({
+    file_path: `${appRootPath}/data/studs/data_z1_03.json`,
+  });
+  let d = Date.now();
+
+  a.data = a.data.map((e) => ({
+    ...e,
+    date: new Date(d - 10 * 60000).toISOString(),
+  }));
+  b.data = b.data.map((e) => ({
+    ...e,
+    date: new Date(d - 20000).toISOString(),
+  }));
+  c.data = c.data.map((e) => ({ ...e, date: new Date(d).toISOString() }));
+
+  a = { new: a.data, old: [] };
+  b = { new: b.data, old: [] };
+  c = { new: c.data, old: [] };
+
+  let ar = process_prev_curr_studs({ new: [], old: [] }, a);
+  console.log("------------");
+  ar = process_prev_curr_studs(ar, b);
+  await delay(2000);
+  console.log("------------");
+  ar = process_prev_curr_studs(ar, c);
+};
+const studs_api_cacher = async (z) => {
+  try {
+    console.log("START studs_api_cacher");
+    let id = "zed-studs-sniper";
+    let z_apis = [1, 2, 3].map((z) => studs_api(z));
+    let api_doc = await Promise.all(z_apis.map((z_api) => studs_api(z_api)));
+    api_doc = _.chain(api_doc).compact().flatten().value() || [];
+    api_doc = struct_studs_api_data(api_doc);
+    if (_.isEmpty(api_doc)) return;
+    api_doc = { new: api_doc, old: [] };
+    let mdb_doc = await zed_db.db.collection("zed_api_cache").findOne({ id });
+    if (_.isEmpty(mdb_doc)) mdb_doc = { new: [], old: [] };
+    let doc = process_prev_curr_studs(mdb_doc, api_doc) || { new: [], old: [] };
+    doc.id = id;
+    await zed_db.db
+      .collection("zed_api_cache")
+      .updateOne({ id }, { $set: doc }, { upsert: true });
+    console.log("DONE  studs_api_cacher");
+  } catch (err) {
+    console.log("ERROR studs_api_cacher");
+  }
 };
 const struct_studs_api_data = (data) => {
+  let date = new Date().toISOString();
   data = data.map((e) => {
     let { bloodline, breed_type, genotype, horse_id: hid, mating_price } = e;
     let name = e.hash_info.name;
@@ -178,6 +203,7 @@ const struct_studs_api_data = (data) => {
       genotype,
       name,
       mating_price,
+      date,
     };
   });
   return data;
@@ -192,11 +218,13 @@ const zed_api_cache_runner = async () => {
 const studs_api_cache_runner = async () => {
   await init();
   await delay(3000);
+  // studs_api_cacher();
   console.log("\n## studs_api_cache_runner started");
   let cron_str = "*/6 * * * * *";
   const c_itvl = cron_parser.parseExpression(cron_str);
   console.log("Next run:", c_itvl.next().toISOString(), "\n");
   cron.schedule(cron_str, () => studs_api_cacher(), cron_conf);
+  console.log("done")
 };
 
 // zed_api_cache_runner()z;
