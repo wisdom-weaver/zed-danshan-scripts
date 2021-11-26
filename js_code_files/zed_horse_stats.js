43f4a3caac120e626c3f101c0e50cd5fb8e07b89
wisdom-weaver <danish.ansari.nox@gmail.com>
2021-11-26 23:22:49 +0530

    horse_stats_all

--- js_code_files/zed_horse_stats.js
+++ js_code_files/zed_horse_stats.js
@@ -41,80 +41,87 @@ const get_roi = (races) => {
 };
 
 const get_horse_stats = async ({ hid }) => {
-  hid = parseInt(hid);
-  let horse = await zedf.horse(hid);
-  let name = horse.hash_info.name;
-  let genotype = horse.genotype;
-  let races = (await get_races_of_hid(hid)) || [];
-  races = _.sortBy(races, "date");
-  let race_n = races.length;
-  let race_first = races[0]?.date || null;
-  let race_last = races[races.length - 1]?.date || null;
-  let paid_races = _.filter(races, (i) => i.fee_tag !== "F") || [];
-  let free_races = _.filter(races, (i) => i.fee_tag === "F") || [];
-  let paid_n = paid_races.length;
+  try {
+    hid = parseInt(hid);
+    let horse = await zedf.horse(hid);
+    let name = horse.hash_info.name;
+    let genotype = horse.genotype;
+    let races = (await get_races_of_hid(hid)) || [];
+    races = _.sortBy(races, "date");
+    let race_n = races.length;
+    let race_first = races[0]?.date || null;
+    let race_last = races[races.length - 1]?.date || null;
+    let paid_races = _.filter(races, (i) => i.fee_tag !== "F") || [];
+    let free_races = _.filter(races, (i) => i.fee_tag === "F") || [];
+    let paid_n = paid_races.length;
 
-  let avg_paid_fee_usd = _.chain(paid_races).map("entryfee_usd").mean().value();
-  let avg_paid_fee_tag = get_fee_tag(avg_paid_fee_usd);
-  let avg_paid_class = _.chain(paid_races).map("thisclass").mean().value();
-  avg_paid_class = Math.round(avg_paid_class);
+    let avg_paid_fee_usd = _.chain(paid_races)
+      .map("entryfee_usd")
+      .mean()
+      .value();
+    let avg_paid_fee_tag = get_fee_tag(avg_paid_fee_usd);
+    let avg_paid_class = _.chain(paid_races).map("thisclass").mean().value();
+    avg_paid_class = Math.round(avg_paid_class);
 
-  let free = {
-    n: free_races.length,
-    per: (free_races.length / race_n) * 100,
-  };
-
-  let paid_races_dist = dists.map((dist) => {
-    if (dist == "all") return [dist, paid_races];
-    let filt = _.filter(paid_races, { distance: dist }) || [];
-    return [dist, filt];
-  });
-  paid_races_dist = _.fromPairs(paid_races_dist);
-
-  for (let dist in paid_races_dist) {
-    if (dist !== "all") dist = parseInt(dist);
-    let filt = paid_races_dist[dist];
-    let n = filt.length;
-    let flame_n = _.filter(filt, { flame: 1 })?.length || 0;
-    let flame_per = (flame_n / n) * 100;
-
-    let roi = get_roi(filt);
-
-    let places_ob = _.entries(places).map(([k, p_range]) => {
-      let p =
-        _.filter(filt, (i) => p_range.includes(i.place.toString()))?.length ||
-        0;
-      let per = (p / n) * 100;
-      let p_ob = { p, per };
-      return [k, p_ob];
-    });
-    places_ob = _.fromPairs(places_ob);
-
-    let ob = {
-      dist,
-      n,
-      roi,
-      places: places_ob,
-      flame_n,
-      flame_per,
+    let free = {
+      n: free_races.length,
+      per: (free_races.length / race_n) * 100,
     };
-    paid_races_dist[dist] = ob;
-  }
 
-  return {
-    hid,
-    name,
-    genotype,
-    race_n,
-    paid_n,
-    race_first,
-    race_last,
-    avg_paid_fee_usd,
-    avg_paid_fee_tag,
-    avg_paid_class,
-    free,
-    paid: paid_races_dist,
-  };
+    let paid_races_dist = dists.map((dist) => {
+      if (dist == "all") return [dist, paid_races];
+      let filt = _.filter(paid_races, { distance: dist }) || [];
+      return [dist, filt];
+    });
+    paid_races_dist = _.fromPairs(paid_races_dist);
+
+    for (let dist in paid_races_dist) {
+      if (dist !== "all") dist = parseInt(dist);
+      let filt = paid_races_dist[dist];
+      let n = filt.length;
+      let flame_n = _.filter(filt, { flame: 1 })?.length || 0;
+      let flame_per = (flame_n / n) * 100;
+
+      let roi = get_roi(filt);
+
+      let places_ob = _.entries(places).map(([k, p_range]) => {
+        let p =
+          _.filter(filt, (i) => p_range.includes(i.place.toString()))?.length ||
+          0;
+        let per = (p / n) * 100;
+        let p_ob = { p, per };
+        return [k, p_ob];
+      });
+      places_ob = _.fromPairs(places_ob);
+
+      let ob = {
+        dist,
+        n,
+        roi,
+        places: places_ob,
+        flame_n,
+        flame_per,
+      };
+      paid_races_dist[dist] = ob;
+    }
+
+    return {
+      hid,
+      name,
+      genotype,
+      race_n,
+      paid_n,
+      race_first,
+      race_last,
+      avg_paid_fee_usd,
+      avg_paid_fee_tag,
+      avg_paid_class,
+      free,
+      paid: paid_races_dist,
+    };
+  } catch (err) {
+    console.log("err horse_stats", hid);
+  }
 };
 const gen_and_upload_horse_stats = async ({ hid }) => {
   let stats = await get_horse_stats({ hid });
@@ -123,10 +130,25 @@ const gen_and_upload_horse_stats = async ({ hid }) => {
     .updateOne({ hid }, { $set: stats }, { upsert: true });
   console.log("done stats", hid);
 };
-const horse_stats_range = async (range) => {
+const horse_stats_range = async (range, cs = 10) => {
   await init();
   await eth_runner_fn();
-  for (let hid of range) await gen_and_upload_horse_stats({ hid });
+  for (let chunk of _.chunk(range, cs)) {
+    let ar = await Promise.all(chunk.map((hid) => get_horse_stats({ hid })));
+    let bulk = [];
+    for (let ob of ar) {
+      if (_.isEmpty(ob)) continue;
+      bulk.push({
+        updateOne: {
+          filter: { hid: ob.hid },
+          update: { $set: ob },
+          upsert: true,
+        },
+      });
+    }
+    await zed_db.db.collection("horse_stats").bulkWrite(bulk);
+    console.log("done", chunk.toString());
+  }
 };
 const horse_stats_all = async () => {
   await init();
@@ -141,4 +163,5 @@ const horse_stats_all = async () => {
 
 module.exports = {
   horse_stats_all,
+  gen_and_upload_horse_stats,
 };
