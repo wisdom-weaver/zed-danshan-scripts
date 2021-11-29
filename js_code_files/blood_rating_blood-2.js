6c847853b29632177e908dbec758b394000344ca
wisdom-weaver <danish.ansari.nox@gmail.com>
2021-11-29 08:03:47 +0530

    rat

--- js_code_files/blood_rating_blood-2.js
+++ js_code_files/blood_rating_blood-2.js
@@ -262,7 +262,7 @@ const runner = async () => {
   // let ob2 = await generate_rating_blood_dist_for_hid(hid);
   // console.log(ob2);
 };
-runner();
+// runner();
 
 module.exports = {
   generate_rating_blood,
