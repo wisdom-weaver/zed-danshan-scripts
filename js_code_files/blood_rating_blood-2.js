5e8ce06d115e77d5091f3572f86b90c06dfded93
wisdom-weaver <danish.ansari.nox@gmail.com>
2021-11-29 08:19:56 +0530

    rat

--- js_code_files/blood_rating_blood-2.js
+++ js_code_files/blood_rating_blood-2.js
@@ -223,9 +223,9 @@ const generate_rating_blood_from_hid = async (hid) => {
 };
 const get_blood_str = (ob) => {
   try {
-    let { cf, d, side, p12_ratio, win_rate, flame_rate, rated_type } = ob;
+    let { cf, d, side, p12_ratio, rat, win_rate, flame_rate, rated_type } = ob;
     if (rated_type == "GH")
-      return `${cf}-${d}-${dec(p12_ratio)}-${dec(win_rate)}-${dec(flame_rate)}`;
+      return `${cf}-${d}-${dec(rat)}-${dec(win_rate)}-${dec(flame_rate)}`;
     return rated_type;
   } catch (err) {
     return "err";
