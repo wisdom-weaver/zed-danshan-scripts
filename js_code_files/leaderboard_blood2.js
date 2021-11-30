834eda323d158eccd1c37201aa2b7770d7c7a4d9
wisdom-weaver <danish.ansari.nox@gmail.com>
2021-12-01 00:13:00 +0530

    leader mongo sort

--- js_code_files/leaderboard_blood2.js
+++ js_code_files/leaderboard_blood2.js
@@ -7,6 +7,7 @@ const {
   read_from_path,
   struct_race_row_data,
   side_text,
+  delay,
 } = require("./utils");
 const app_root = require("app-root-path");
 const { dec, dec_per } = require("./utils");
@@ -78,71 +79,168 @@ const get_downloaded_horses_data = async () => {
   console.log("got", _.keys(mapped).length);
   return mapped;
 };
-const generate_leaderboard_b2 = async (down = 1, only = null, cs = 100) => {
+const generate_leaderboard_b2 = async (only = null, cs = 100) => {
   await initiate();
   await fix_empty_names();
-  if (down == 1) await leaderboard_download();
-  let mapped = await get_downloaded_horses_data();
-  console.log(mapped[3]);
+  // if (down == 1) await leaderboard_download();
+  // let mapped = await get_downloaded_horses_data();
+  // console.log(mapped[3]);
   let dists = ["S", "M", "D", "All"];
   if (only) dists = [only];
   for (let dist of dists) {
-    await generate_leaderboard_b2_each_dist({ mapped, dist, cs });
+    await generate_leaderboard_b2_each_dist({ dist, cs });
   }
 };
-const generate_leaderboard_b2_each_dist = async ({ mapped, dist, cs }) => {
-  console.log("#DIST", dist);
-  mapped = _.chain(mapped)
-    .values()
-    .map((i) => {
-      if (_.isEmpty(i) || _.isEmpty(i[dist])) return null;
-      let { hid, name } = i;
-      let ob = { hid, name, ...i[dist] };
-      return ob;
-    })
-    .value();
+// const generate_leaderboard_b2_each_dist = async ({ mapped, dist, cs }) => {
+//   console.log("#DIST", dist);
+//   mapped = _.chain(mapped)
+//     .values()
+//     .map((i) => {
+//       if (_.isEmpty(i) || _.isEmpty(i[dist])) return null;
+//       let { hid, name } = i;
+//       let ob = { hid, name, ...i[dist] };
+//       return ob;
+//     })
+//     .value();
 
-  let gh_s = _.filter(mapped, { rated_type: "GH" });
-  let ch_s = _.filter(mapped, { rated_type: "CH" });
-  let nr_s = _.filter(mapped, { rated_type: "NR" });
+//   let gh_s = _.filter(mapped, { rated_type: "GH" });
+//   let ch_s = _.filter(mapped, { rated_type: "CH" });
+//   let nr_s = _.filter(mapped, { rated_type: "NR" });
 
-  let ar = [];
-  ar = _.chain(gh_s)
-    .orderBy(
-      [
-        (i) => {
-          return i.cf;
-        },
-        (i) => {
-          return +i.rat;
-        },
-        (i) => {
-          return +i.win_rate;
-        },
-        (i) => {
-          return +i.flame_rate;
-        },
-      ],
-      ["asc", "desc", "desc", "desc"]
-    )
-    .map((i, idx) => {
-      let str = get_blood_str(i);
-      return { ...i, rank: idx + 1, str };
-    })
-    .value();
-  ar = ar.concat(_.map(ch_s, (i) => ({ ...i, rank: null })));
-  ar = ar.concat(_.map(nr_s, (i) => ({ ...i, rank: null })));
-  console.table(
-    _.chain(ar)
-      .map((i) => {
-        return { ...i };
-      })
-      .value()
-      .slice(0, 100)
-  );
+//   let ar = [];
+//   ar = _.chain(gh_s)
+//     .orderBy(
+//       [
+//         (i) => {
+//           return i.cf;
+//         },
+//         (i) => {
+//           return +i.rat;
+//         },
+//         (i) => {
+//           return +i.win_rate;
+//         },
+//         (i) => {
+//           return +i.flame_rate;
+//         },
+//       ],
+//       ["asc", "desc", "desc", "desc"]
+//     )
+//     .map((i, idx) => {
+//       let str = get_blood_str(i);
+//       return { ...i, rank: idx + 1, str };
+//     })
+//     .value();
+//   ar = ar.concat(_.map(ch_s, (i) => ({ ...i, rank: null })));
+//   ar = ar.concat(_.map(nr_s, (i) => ({ ...i, rank: null })));
+//   console.table(
+//     _.chain(ar)
+//       .map((i) => {
+//         return { ...i };
+//       })
+//       .value()
+//       .slice(0, 100)
+//   );
 
-  let leader_ar = _.map(ar.slice(0, 100), (i) => {
-    let { rank, hid, cf, d, str, name } = i;
+//   let leader_ar = _.map(ar.slice(0, 100), (i) => {
+//     let { rank, hid, cf, d, str, name } = i;
+//     return {
+//       hid,
+//       name,
+//       rank,
+//       horse_rating: {
+//         cf,
+//         d,
+//         str,
+//         type: dist == "All" ? "all" : "dist",
+//       },
+//     };
+//   });
+//   let now = Date.now();
+//   let leader_doc = {
+//     id: `leaderboard-${dist}`,
+//     date: now,
+//     date_str: new Date(now).toISOString(),
+//     db_date: new Date(now).toISOString(),
+//     "leaderboard-dist": dist.toString(),
+//     leaderboard: leader_ar,
+//   };
+//   // console.log(leader_doc);
+//   await zed_db.db
+//     .collection("leaderboard")
+//     .updateOne({ id: leader_doc.id }, { $set: leader_doc }, { upsert: true });
+//   console.log("written", dist, "leaderboard\n----------------\n");
+
+//   let bulk = [];
+//   await zed_db.db
+//     .collection("rating_blood_dist")
+//     .updateMany({}, { $set: { [`${dist}.rank`]: null } });
+//   console.log("cleared, ranks");
+//   let to_upd = _.filter(ar, (i) => i.rank !== null);
+//   console.log("need to write ranks", to_upd.length);
+//   for (let doc of to_upd) {
+//     let { hid, rank } = doc;
+//     if (!hid) continue;
+//     bulk.push({
+//       updateOne: {
+//         filter: { hid },
+//         update: { $set: { [`${dist}.rank`]: rank } },
+//       },
+//     });
+//   }
+//   let i = 0;
+//   for (let mini_bulk of _.chunk(bulk, cs)) {
+//     if (_.isEmpty(bulk)) continue;
+//     await zed_db.db.collection("rating_blood_dist").bulkWrite(mini_bulk);
+//     i += mini_bulk.length;
+//     console.log("rating_blood_dist ranks:", dec_per(i, bulk.length));
+//   }
+// };
+
+const leader_query = (dist = null, need_details = 0, limit = null) => {
+  if (!dist) [{ $match: { hid: -1 } }];
+  let ar = [
+    { $match: { [`${dist}.rated_type`]: "GH" } },
+    {
+      $sort: {
+        [`${dist}.cf`]: 1,
+        [`${dist}.rat`]: -1,
+        [`${dist}.win_rate`]: -1,
+        [`${dist}.flame_rate`]: -1,
+      },
+    },
+    ...(limit ? [{ $limit: limit }] : []),
+    {
+      $project: {
+        _id: 0,
+        hid: 1,
+        name: 1,
+        ...(need_details ? { [dist]: 1 } : {}),
+        // [`${dist}.cf`]: 1,
+        // [`${dist}.d`]: 1,
+        // [`${dist}.rat`]: 1,
+        // [`${dist}.win_rate`]: 1,
+        // [`${dist}.flame_rate`]: 1,
+        // [`${dist}.rank`]: 1,
+        // [`${dist}.rated_type`]: 1,
+      },
+    },
+  ];
+  return ar;
+};
+
+const generate_leaderboard_b2_each_dist = async ({ dist = null, cs }) => {
+  if (!dist) return;
+  console.log(dist);
+  let docs = await zed_db.db
+    .collection("rating_blood_dist")
+    .aggregate(leader_query(dist, 1, 100))
+    .toArray();
+  let leader_ar = _.map(docs, (i, idx) => {
+    let { hid, name } = i;
+    let { cf, d } = i[dist];
+    let str = get_blood_str(i[dist]);
+    let rank = idx + 1;
     return {
       hid,
       name,
@@ -164,50 +262,39 @@ const generate_leaderboard_b2_each_dist = async ({ mapped, dist, cs }) => {
     "leaderboard-dist": dist.toString(),
     leaderboard: leader_ar,
   };
-  // console.log(leader_doc);
+  console.table(leader_ar);
   await zed_db.db
     .collection("leaderboard")
     .updateOne({ id: leader_doc.id }, { $set: leader_doc }, { upsert: true });
-  console.log("written", dist, "leaderboard\n----------------\n");
+  console.log(dist, "done");
 
-  let bulk = [];
-  await zed_db.db
-    .collection("rating_blood_dist")
-    .updateMany({}, { $set: { [`${dist}.rank`]: null } });
-  console.log("cleared, ranks");
-  let to_upd = _.filter(ar, (i) => i.rank !== null);
-  console.log("need to write ranks", to_upd.length);
-  for (let doc of to_upd) {
-    let { hid, rank } = doc;
-    if (!hid) continue;
-    bulk.push({
-      updateOne: {
-        filter: { hid },
-        update: { $set: { [`${dist}.rank`]: rank } },
-      },
-    });
-  }
-  let i = 0;
-  for (let mini_bulk of _.chunk(bulk, cs)) {
-    if (_.isEmpty(bulk)) continue;
-    await zed_db.db.collection("rating_blood_dist").bulkWrite(mini_bulk);
-    i += mini_bulk.length;
-    console.log("rating_blood_dist ranks:", dec_per(i, bulk.length));
-  }
+  await write_ranks({ dist, cs });
 };
 
-const gene_leader = async () => {
-  await init()
-  let dist = "All";
-  let docs = await zed_db.db
-    .collection("rating_blood_dist")
-    .aggregate([
-      { $match: { [`${dist}.rated_type`]: "urgent" } },
-      { $sort: { [`${dist}.cf`]: 1 } },
-    ])
-    .toArray();
-  console.log(docs);
-  return [];
+const write_ranks = async ({ dist, cs }) => {
+  try {
+    let docs = await zed_db.db
+      .collection("rating_blood_dist")
+      .aggregate(leader_query(dist, 0, null));
+    let i = 0;
+    let cur = docs;
+    let last_cur;
+    while (true) {
+      let last_cur = _.cloneDeep(cur);
+      try {
+        if (!cur.hasNext()) break;
+        let doc = await cur.next();
+        let rank = ++i;
+        console.log(rank, doc);
+      } catch (err) {
+        cur = last_cur;
+        console.log("...err");
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 };
 
 const fix_empty_names = async () => {
@@ -250,8 +337,7 @@ const runner = async () => {
   await generate_leaderboard_b2();
   console.log("done");
 };
-// runner();
-// gene_leader();
+runner();
 
 module.exports = {
   generate_leaderboard_b2,
