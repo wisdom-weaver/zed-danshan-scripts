c55a98df3f0020ab0e04c7731cce7ed22fe5656a
wisdom-weaver <danish.ansari.nox@gmail.com>
2022-01-06 10:16:36 +0530

    dan_max_gap

--- code/dan/max_gap.js
+++ code/dan/max_gap.js
@@ -16,8 +16,8 @@ const download_races = async (st, ed) => {
 };
 
 const run_duration = async (st, ed) => {
-  st = new Date(st);
-  ed = new Date(ed);
+  st = new Date(st).toISOString();
+  ed = new Date(ed).toISOString();
   let races = await download_races(st, ed);
   races = _.keyBy(races, "raceid");
   console.log("found", _.keys(races).length);
