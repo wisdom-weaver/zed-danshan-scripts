def7d0f84b3a3a5861b46f926d70b51eb8dda597
wisdom-weaver <danish.ansari.nox@gmail.com>
2022-01-06 10:33:58 +0530

    dan_max_gap

--- code/dan/max_gap.js
+++ code/dan/max_gap.js
@@ -23,13 +23,13 @@ const run_duration = async (st, ed) => {
   console.log("found", _.keys(races).length);
   let data = [];
   for (let [rid, ar] of _.entries(races)) {
-    console.log(ar)
+    console.log(rid, ar.length);
     let f_ob = _.chain(ar).keyBy("place").mapValues("flame").value();
     let hid_ob = _.chain(ar).keyBy("place").mapValues("hid").value();
     let t_ob = _.chain(ar).keyBy("place").mapValues("finishtime").value();
-    console.log(f_ob);
-    console.log(hid_ob);
-    console.log(t_ob);
+    // console.log(f_ob);
+    // console.log(hid_ob);
+    // console.log(t_ob);
     let g_1_2 = t_ob[2] - t_ob[1];
     let g_11_12 = t_ob[12] - t_ob[11];
     let hid_1 = hid_ob[1];
