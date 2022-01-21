19152a05bb1f81383d3da6694504a9839f24348b
wisdom-weaver <danish.ansari.nox@gmail.com>
2022-01-22 02:28:46 +0530

    br satble ifx

--- code/v3/horses.js
+++ code/v3/horses.js
@@ -237,11 +237,11 @@ const get_missings = async (range) => {
     let hids4 = await get_valid_hids_in_coll(chunk_hids, "rating_flames3");
     let hids5 = await get_valid_hids_in_ancestry(chunk_hids);
 
-    console.log("hids1:",hids1.length)
-    console.log("hids2:",hids2.length)
-    console.log("hids3:",hids3.length)
-    console.log("hids4:",hids4.length)
-    console.log("hids5:",hids5.length)
+    console.log("hids1:", hids1.length);
+    console.log("hids2:", hids2.length);
+    console.log("hids3:", hids3.length);
+    console.log("hids4:", hids4.length);
+    console.log("hids5:", hids5.length);
 
     let hids_exists = _.intersection(hids1, hids2, hids3, hids4, hids5);
     console.log("hids_exists", hids_exists.length);
@@ -368,10 +368,11 @@ const fix_unnamed_cron = async () => {
 const fix_stable_h1 = async (hid) => {
   hid = parseInt(hid);
   let doc = await zedf.horse(hid);
+  let name = doc.name;
   let oid = doc.owner;
   let stable_name = doc.owner_stable;
   let slug = doc.owner_stable_slug;
-  return { hid, oid, stable_name, slug };
+  return { hid, name, oid, stable_name, slug };
 };
 
 const fix_stable = () =>
