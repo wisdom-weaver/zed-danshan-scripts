68489e05f9542d017227450053d7c4642cf5b635
wisdom-weaver <danish.ansari.nox@gmail.com>
2022-01-23 23:58:40 +0530

    runner race_horses

--- code/races/race_horses.js
+++ code/races/race_horses.js
@@ -5,8 +5,8 @@ const cron = require("node-cron");
 const cron_parser = require("cron-parser");
 const utils = require("../utils/utils");
 
-const def_cs = 200;
-const run_cs = 25;
+const def_cs = 300;
+const run_cs = 30;
 
 const coll = "race_horses";
 const push = async (ob) => {
@@ -57,7 +57,7 @@ const run = async (cs = def_cs) => {
 };
 
 const run_cron = (cs = def_cs) => {
-  let cron_str = "*/30 * * * * *";
+  let cron_str = "0 */1 * * * *";
   const c_itvl = cron_parser.parseExpression(cron_str);
   console.log("Next run:", c_itvl.next().toISOString(), "\n");
   cron.schedule(cron_str, () => run(cs), utils.cron_conf);
