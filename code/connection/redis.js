de33ada09f431a87fe40080a8a8d6dda4d2fa227
wisdom-weaver <danish.ansari.nox@gmail.com>
2022-06-13 21:37:59 +0530

    race horses run stats

--- code/connection/redis.js
+++ code/connection/redis.js
@@ -1,4 +1,4 @@
 const Redis = require("ioredis");
-const rurl = "127.0.0.1:6379";
-const redis = new Redis(rurl);
-module.exports = { redis };
+// const rurl = "127.0.0.1:6379";
+// const redis = new Redis(rurl);
+module.exports = { redis:null };
